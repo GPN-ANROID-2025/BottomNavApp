96cd8b2192480c09569982564f50154d02acf6e1
sumagodev1 <sumagodev1@gmail.com>
2025-02-11 11:00:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/bottomnavapp/SecondActivity.java
@@ -0,0 +1,40 @@
+package com.example.bottomnavapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        navigationView=findViewById(R.id.bottomNavigationView);
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Toast.makeText(SecondActivity.this, ""+item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+}
